b32c856eeda87f58d294748462a8d8c13450a296
Gençay Yıldız <gyildizmail@gmail.com>
2023-06-26 01:05:55 +0300

    Add project files.

---
+++ AWS.CloudWatch.Serilog.Example/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AWS.CloudWatch.Serilog.Example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+            => _logger = logger;
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogDebug("Debug Log");
+            _logger.LogInformation("Information Log");
+            _logger.LogError("Error Log");
+            return Ok();
+        }
+    }
+}
---
+++ AWS.CloudWatch.Serilog.Example/Program.cs
@@ -0,0 +1,27 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Host.UseSerilog((_, logger) => logger.WriteTo.Console().ReadFrom.Configuration(builder.Configuration));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
